e2bf02b237025af39824980c370ca85d26161150
Florian Bachofner <florianbachofner@hotmail.com>
2019-07-20 14:51:06 +0200

    second commit

---
+++ script.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+webpage_response = requests.get('https://themilzandgryllzshow.podbean.com/')
+
+webpage = webpage_response.content
+
+soup = BeautifulSoup(webpage, "html.parser")
+
+
+print(soup.select("#home_visitor_stat")[0].get_text())
+
+#python ~/Documents/6_Webdesign/09_Dreamweaver/themilzandgryllzshow
